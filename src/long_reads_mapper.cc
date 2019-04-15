3e72bbc3cdec6cd6e8f2848cfc254d2f49e1c27a
kpongracic <kp50063@fer.hr>
2019-04-16 01:11:41 +0200

    Added prep_ref and index_ref

--- src/long_reads_mapper.cc
+++ src/long_reads_mapper.cc
@@ -26,6 +26,12 @@ const std::set<std::string> fasta_formats = {".fasta", ".fa", ".fasta.gz", ".fa.
 const std::set<std::string> fastq_formats = {".fastq", ".fq", ".fastq.gz", ".fq.gz"};
 
 typedef std::tuple<unsigned int, unsigned int, bool> minimizer;
+typedef std::tuple<unsigned int, unsigned int, bool> triplet_t;
+
+// position, amount
+typedef std::pair<unsigned int, unsigned int> minimizer_index_t;
+// query position, reference position, relative strand
+typedef std::tuple<unsigned int, unsigned int, bool> minimizer_hit_t;
 
 static struct option long_options[] = {
   {"help", no_argument, NULL, 'h'},
@@ -158,6 +164,280 @@ bool contains_extension(const std::string file, const std::set<std::string> &ext
   return false;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void prep_ref(std::vector<triplet_t>& t_minimizers, const float f) {
+  std::unordered_map<unsigned int, unsigned int> ref_min_frequency;
+  for (const auto& minimizer : t_minimizers) {
+    ref_min_frequency[std::get<0>(minimizer)]++;
+  }
+
+  std::vector<unsigned int> occurences;
+  occurences.reserve(ref_min_frequency.size());
+
+  for (const auto& entry : ref_min_frequency) {
+    occurences.push_back(entry.second);
+  }
+
+  unsigned int position = (unsigned int)((1.0f - f) * (occurences.size() - 1.0f));
+  std::sort(occurences.begin(), occurences.end());
+
+  unsigned int cutoff_freq = occurences[position] == 1 ? 2 : occurences[position];
+
+  std::vector<triplet_t> temp;
+  temp.reserve(t_minimizers.size());
+
+  for (const auto& minimizer : t_minimizers) {
+    if (ref_min_frequency[std::get<0>(minimizer)] < cutoff_freq) {
+      temp.push_back(minimizer);
+    }
+  }
+
+  std::swap(t_minimizers, temp);
+
+  std::sort(t_minimizers.begin(), t_minimizers.end(),
+      [] (const triplet_t& a, const triplet_t& b) {
+        return (std::get<0>(a) < std::get<0>(b));
+      });
+
+  t_minimizers.shrink_to_fit();
+}
+
+
+std::unordered_map<unsigned int, minimizer_index_t> index_ref(
+    const std::vector<triplet_t>& t_minimizers) {
+
+  std::unordered_map<unsigned int, minimizer_index_t> ref_index;
+  unsigned int pos = 0;
+  unsigned int num = 0;
+  unsigned int prev_min = std::get<0>(t_minimizers[0]);
+  for (const auto& minimizer : t_minimizers) {
+    if (prev_min != std::get<0>(minimizer)) {
+      ref_index[prev_min] = std::make_pair(pos, num);
+      pos += num;
+      num = 1;
+      prev_min = std::get<0>(minimizer); 
+    } else {
+      num++;
+    }
+  }
+  ref_index[prev_min] = std::make_pair(pos, num);
+
+  return ref_index;
+}
+
+
+std::unordered_map<unsigned int, std::vector<minimizer_hit_t>> find_minimizer_hits(
+    const std::unordered_map<unsigned int, minimizer_index_t>& ref_index,
+    const std::vector<triplet_t>& t_minimizers,
+    const std::vector<triplet_t>& q_minimizers,
+    int k_value) {
+
+  std::unordered_map<unsigned int, minimizer_hit_t> hits;
+
+  for (const auto& minimizer : q_minimizers) {
+    auto found = ref_index.find(std::get<0>(minimizer));
+    if (found == ref_index.end()) continue;
+
+    for (unsigned int i = 0; i < found->second.second; i++){
+      bool srand = false;
+      if (std::get<2>(minimizer) == std::get<2>(t_minimizers[found->second.first + i])) bool = true;
+      int position = std::get<1>(t_minimizers[found->second.first + i]);
+
+      if(position < k_value){
+        hits[0].emplace_back(std::get<1>(minimizer),
+          std::get<1>(t_minimizers[found->second.first + i]), srand);
+      }else{
+        int key = position / k_value;
+        hits[key].emplace_back(std::get<1>(minimizer),
+          std::get<1>(t_minimizers[found->second.first + i]), srand);
+        hits[key - 1].emplace_back(std::get<1>(minimizer),
+          std::get<1>(t_minimizers[found->second.first + i]), srand);
+      }
+    }
+  }
+  return hits;
+}
+
+std::string map_paf(const std::vector<triplet_t>& t_minimizers,
+    const std::unordered_map<unsigned int, minimizer_index_t>& ref_index,
+    const std::vector<std::unique_ptr<FastAQ>>& fastaq_objects1,
+    const std::vector<std::unique_ptr<FastAQ>>& fastaq_objects2,
+    unsigned int k, unsigned int window_length, brown::AlignmentType alignment,
+    int match, int mismatch, int gap,
+    unsigned int t_begin, unsigned int t_end, int bw, bool c, int k_value) {
+
+  std::string paf;
+
+
+  for (unsigned int fobj = t_begin; fobj < t_end; fobj++) {
+
+    if(fastaq_objects1[fobj]->sequence.size() < 2 * k_value) continue;
+
+    std::vector<triplet_t> q_minimizers = brown::minimizers(
+      fastaq_objects1[fobj]->sequence.c_str(), fastaq_objects1[fobj]->sequence.size(), k, window_length);
+
+    std::unordered_map<unsigned int, minimizer_hit_t> hits = find_minimizer_hits(ref_index, t_minimizers, q_minimizers, k_value);
+
+
+    
+
+
+    std::sort(hits.begin(), hits.end(), hit_ordering);
+
+    std::vector<std::pair<region_t, unsigned int>> regions;
+
+    unsigned int b = 0;
+    for (unsigned int e = 0; e < hits.size(); ++e) {
+      if (e == hits.size() - 1 ||
+          std::get<2>(hits[e + 1]) != std::get<2>(hits[e]) ||
+          std::abs(band(hits[e + 1], hits[e])) >= bw) {
+        std::pair<region_t, unsigned int> temp_region = find_region(hits, b, e, k);
+        if (temp_region.second > 4 * k) {
+          regions.push_back(temp_region);
+        }
+        b = e + 1;
+      }
+    }
+
+    if (regions.empty()) {
+      continue;
+    }
+
+    merge_regions(regions, bw);
+
+    std::sort(regions.begin(), regions.end(),
+        [] (const std::pair<region_t, unsigned int>& a,
+            const std::pair<region_t, unsigned int>& b) {
+      return a.second > b.second;
+    });
+
+    for (const auto& reg : regions) {
+      region_t max_region = reg.first;
+      unsigned int matches = reg.second;
+
+      std::string rel_strand;
+      unsigned int total;
+      unsigned int q_b, q_e, t_b, t_e;
+      std::string cigar;
+      unsigned int target_begin;
+
+      t_b = std::get<1>(max_region.first);
+      t_e = std::get<1>(max_region.second) + k;
+      q_b = std::get<0>(max_region.first);
+      q_e = std::get<0>(max_region.second) + k;
+      total = (t_e - t_b);
+      
+      if (!std::get<2>(max_region.first)) {
+        rel_strand = "+";
+        if (c) {
+          brown::pairwise_alignment(fastaq_objects1[fobj]->sequence.c_str() + q_b,
+              q_e - q_b,
+              fastaq_objects2[0]->sequence.c_str() + t_b,
+              t_e - t_b,
+              alignment, match, mismatch, gap, cigar, target_begin);
+          matches = read_cigar(cigar, total);
+        }
+      } else {
+        rel_strand = "-";
+        if (c) {
+          std::string rc = reverse_complement(fastaq_objects1[fobj]->sequence, q_b,
+              q_e - q_b);
+          brown::pairwise_alignment(rc.c_str(), rc.size(),
+              fastaq_objects2[0]->sequence.c_str() + t_b,
+              t_e - t_b,
+              alignment, match, mismatch, gap, cigar, target_begin);
+          matches = read_cigar(cigar, total);
+        }
+      }
+      paf += fastaq_objects1[fobj]->name + "\t" +
+             std::to_string(fastaq_objects1[fobj]->sequence.size()) + "\t" +
+             std::to_string(q_b) + "\t" +
+             std::to_string(q_e) + "\t" +
+             rel_strand + "\t" +
+             fastaq_objects2[0]->name + "\t" +
+             std::to_string(fastaq_objects2[0]->sequence.size()) + "\t" +
+             std::to_string(t_b) + "\t" +
+             std::to_string(t_e) + "\t" +
+             std::to_string(matches) + "\t" +
+             std::to_string(total) + "\t" +
+             std::to_string(255);
+      if (c) {
+        paf += "\tcg:Z:" + cigar;
+      }
+      paf += "\n";
+    }
+  }
+  return paf;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main (int argc, char **argv) {
   int optchr;
 
@@ -268,90 +548,138 @@ int main (int argc, char **argv) {
 
   int k_value = 1000;
 
-  std::multimap<unsigned int, int> rm;
-
-  for(int i = 0; i < size ; i = i + k_value){
-    int length = 2 * k_value;
-    if(i + length > size) length = size - i;
-    std::string tmp = fastaq_objects1[0]->sequence.substr(i, length);
-    std::vector<minimizer> minimizers = brown::minimizers(tmp.c_str(), length, k, window_length);
+  
 
 
-    for(auto& minimizer : minimizers){
-      rm.insert({std::get<0>(minimizer), i / 1000});
-    }
+
+
+
+
+
+
+
+
+
+  std::vector<triplet_t> t_minimizers = brown::minimizers(
+      fastaq_objects2[0]->sequence.c_str(), fastaq_objects2[0]->sequence.size(), k, window_length);
+
+  prep_ref(t_minimizers, f);
+
+  std::unordered_map<unsigned int, minimizer_index_t> ref_index = index_ref(t_minimizers);
+
+  std::shared_ptr<thread_pool::ThreadPool> thread_pool = thread_pool::createThreadPool(t);
+
+  std::vector<std::future<std::string>> thread_futures;
+
+  for (unsigned int tasks = 0; tasks < t - 1; ++tasks) {
+    thread_futures.emplace_back(thread_pool->submit_task(map_paf, std::ref(t_minimizers), std::ref(ref_index),
+      std::ref(fastaq_objects1), std::ref(fastaq_objects2),
+      k, window_length, alignment, match, mismatch, gap, tasks * fastaq_objects1.size() / t, (tasks + 1) * fastaq_objects1.size() / t, bw, c, k_value));  
+  }
+  thread_futures.emplace_back(thread_pool->submit_task(map_paf, std::ref(t_minimizers), std::ref(ref_index),
+      std::ref(fastaq_objects1), std::ref(fastaq_objects2),
+      k, window_length, alignment, match, mismatch, gap, (t - 1) * fastaq_objects1.size() / t, fastaq_objects1.size(), bw, c, k_value));
+  
+  for (auto& it : thread_futures) {
+    it.wait();
+    std::cout << it.get();
   }
 
-  printf("Velicina rm mape : %d\n\n", rm.size());
 
-  for(int i = 0; i <  100 /*fastaq_objects2.size()*/; i++){
-    int sequence_length = fastaq_objects2[i]->sequence.size();
 
-    printf("Sekvenca br. %d, duljina sekvence : %d\n", i, sequence_length);
+
+
+
+
+
+
+
+
+
+  // std::multimap<unsigned int, int> rm;
+
+  // for(int i = 0; i < size ; i = i + k_value){
+  //   int length = 2 * k_value;
+  //   if(i + length > size) length = size - i;
+  //   std::string tmp = fastaq_objects1[0]->sequence.substr(i, length);
+  //   std::vector<minimizer> minimizers = brown::minimizers(tmp.c_str(), length, k, window_length);
+
+
+  //   for(auto& minimizer : minimizers){
+  //     rm.insert({std::get<0>(minimizer), i / 1000});
+  //   }
+  // }
+
+  // printf("Velicina rm mape : %d\n\n", rm.size());
+
+  // for(int i = 0; i <  100 /*fastaq_objects2.size()*/; i++){
+  //   int sequence_length = fastaq_objects2[i]->sequence.size();
+
+  //   printf("Sekvenca br. %d, duljina sekvence : %d\n", i, sequence_length);
     
-    if(sequence_length < 2 * k_value){
-      printf("Preskacem\n");
-      printf("----------------------------\n\n");
-      continue;
-    }
+  //   if(sequence_length < 2 * k_value){
+  //     printf("Preskacem\n");
+  //     printf("----------------------------\n\n");
+  //     continue;
+  //   }
 
-    std::vector<minimizer> start_minimizers = brown::minimizers(fastaq_objects2[i]->sequence.c_str(), k_value, k, window_length);
-    std::vector<minimizer> end_minimizers = brown::minimizers(fastaq_objects2[i]->sequence.substr(sequence_length - k_value - 1, k_value).c_str(), k_value, k, window_length);
+  //   std::vector<minimizer> start_minimizers = brown::minimizers(fastaq_objects2[i]->sequence.c_str(), k_value, k, window_length);
+  //   std::vector<minimizer> end_minimizers = brown::minimizers(fastaq_objects2[i]->sequence.substr(sequence_length - k_value - 1, k_value).c_str(), k_value, k, window_length);
 
-    printf("Broj start_minimizers: %d , broj end_minimizers: %d \n", start_minimizers.size(), end_minimizers.size());
-    printf("Pocetak:\n");
+  //   printf("Broj start_minimizers: %d , broj end_minimizers: %d \n", start_minimizers.size(), end_minimizers.size());
+  //   printf("Pocetak:\n");
 
-    //kopija koda
-    std::unordered_map<int, int> best;
-    for(auto& minimizer : start_minimizers){
+  //   //kopija koda
+  //   std::unordered_map<int, int> best;
+  //   for(auto& minimizer : start_minimizers){
 
-      auto itr1 = rm.lower_bound(std::get<0>(minimizer)); 
-      auto itr2 = rm.upper_bound(std::get<0>(minimizer)); 
+  //     auto itr1 = rm.lower_bound(std::get<0>(minimizer)); 
+  //     auto itr2 = rm.upper_bound(std::get<0>(minimizer)); 
       
-      while(itr1 != itr2){
-        best[itr1->second]++;
-        itr1++;
-      } 
-    }
+  //     while(itr1 != itr2){
+  //       best[itr1->second]++;
+  //       itr1++;
+  //     } 
+  //   }
 
-    int max = 0;
-    int pos = 0;
-    for(auto& pair : best){
-      if(pair.second > max){
-        pos = pair.first;
-        max = pair.second;
-      }
-      if(pair.second > 40) printf("Regija %d ima %d pogodaka\n", pair.first, pair.second);
-    }
-    printf("Najbolja je %d regija, s %d pogodaka\n", pos, max);
+  //   int max = 0;
+  //   int pos = 0;
+  //   for(auto& pair : best){
+  //     if(pair.second > max){
+  //       pos = pair.first;
+  //       max = pair.second;
+  //     }
+  //     if(pair.second > 40) printf("Regija %d ima %d pogodaka\n", pair.first, pair.second);
+  //   }
+  //   printf("Najbolja je %d regija, s %d pogodaka\n", pos, max);
 
-    printf("Kraj:\n");
+  //   printf("Kraj:\n");
     
-    //kopija koda
-    std::unordered_map<int, int> best2;
-    for(auto& minimizer : end_minimizers){
+  //   //kopija koda
+  //   std::unordered_map<int, int> best2;
+  //   for(auto& minimizer : end_minimizers){
 
-      auto itr1 = rm.lower_bound(std::get<0>(minimizer)); 
-      auto itr2 = rm.upper_bound(std::get<0>(minimizer)); 
+  //     auto itr1 = rm.lower_bound(std::get<0>(minimizer)); 
+  //     auto itr2 = rm.upper_bound(std::get<0>(minimizer)); 
       
-      while(itr1 != itr2){
-        best2[itr1->second]++;
-        itr1++;
-      } 
-    }
+  //     while(itr1 != itr2){
+  //       best2[itr1->second]++;
+  //       itr1++;
+  //     } 
+  //   }
 
-    max = 0;
-    pos = 0;
-    for(auto& pair : best2){
-      if(pair.second > max){
-        pos = pair.first;
-        max = pair.second;
-      }
-      if(pair.second > 40) printf("Regija %d ima %d pogodaka\n", pair.first, pair.second);
-    }
-    printf("Najbolja je %d regija, s %d pogodaka\n", pos, max);
-    printf("----------------------------\n\n");
-  }
+  //   max = 0;
+  //   pos = 0;
+  //   for(auto& pair : best2){
+  //     if(pair.second > max){
+  //       pos = pair.first;
+  //       max = pair.second;
+  //     }
+  //     if(pair.second > 40) printf("Regija %d ima %d pogodaka\n", pair.first, pair.second);
+  //   }
+  //   printf("Najbolja je %d regija, s %d pogodaka\n", pos, max);
+  //   printf("----------------------------\n\n");
+  // }
 
 
   return 0;
