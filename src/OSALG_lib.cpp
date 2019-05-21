2c1148481481aa391d0d75a26b10307c6e3a4135
RJPenic <rafael-josip.penic@fer.hr>
2019-05-22 00:02:02 +0200

    Returned SAVE2_type entries and secondary list

--- src/OSALG_lib.cpp
+++ src/OSALG_lib.cpp
@@ -15,12 +15,26 @@ namespace OSALG {
 	std::vector<int> u{4, 2};
 	std::vector<int> v{1, 13};
 
+	struct save2 {
+		int p;
+		int q;
+
+		save2(int p, int q) : p(p), q(q)
+		{}
+
+		save2()
+		{}
+	} typedef SAVE2_type;//secondary list entries
+
 	struct save1 {
 		int m;
 		int n;
-		int p;
+		SAVE2_type pointer;
+	
+		save1(int m, int n, SAVE2_type pointer) : m(m), n(n), pointer(pointer)
+		{}
 
-		save1(int m, int n, int p) : m(m), n(n), p(p)
+		save1(int m, int n, int p, int q) : m(m), n(n), pointer(p, q)
 		{}
 
 		save1()
@@ -31,7 +45,7 @@ namespace OSALG {
 		int d_arr_val = 0;
 		int f_arr[L + 2];
 		bool e_arr[L + 2];
-		int p_arr[L + 2];
+		SAVE2_type p_arr[L + 2];
 	} typedef alignment_info;
 
 	void init_first_row(std::vector<alignment_info> &row) {
@@ -42,11 +56,13 @@ namespace OSALG {
 			row[0].f_arr[i] = std::numeric_limits<int>::max();
 		}
 
-		row[0].p_arr[0] = 0;
+		row[0].p_arr[0].p = 0;
+		row[0].p_arr[0].q = -1;
 		row[0].e_arr[0] = true;
 
 		for (int i = 1; i <= L + 1; ++i) {
-			row[0].p_arr[i] = -1;
+			row[0].p_arr[i].p = -1;
+			row[0].p_arr[i].q = -1;
 		}
 		//--------------
 
@@ -64,7 +80,8 @@ namespace OSALG {
 
 			for (int k = 0; k <= L + 1; ++k) {
 				row[i].e_arr[k] = (row[i].d_arr_val == row[i].f_arr[k]);
-				row[i].p_arr[k] = (row[i].e_arr[k]) ? 0 : -1;
+				row[i].p_arr[k].p = (row[i].e_arr[k]) ? 0 : -1;
+				row[i].p_arr[k].q = -1;
 			}
 		}
 	}
@@ -87,18 +104,28 @@ namespace OSALG {
 
 		for (int k = 0; k <= L + 1; ++k) {
 			current_row[0].e_arr[k] = (current_row[0].d_arr_val == current_row[0].f_arr[k]);
-			current_row[0].p_arr[k] = (current_row[0].e_arr[k]) ? 0 : -1;
+			current_row[0].p_arr[k].p = (current_row[0].e_arr[k]) ? 0 : -1;
+			current_row[0].p_arr[k].q = -1;
 		}
 	}
 
-	void adr_function(int &pp, int m, int n, std::vector<alignment_info> &row, std::vector<SAVE1_type> &primary_list) {
-		pp = primary_list.size();
+	void adr_function(SAVE2_type &pp, int m, int n, std::vector<alignment_info> &row, std::vector<SAVE1_type> &primary_list) {
+		pp.p = primary_list.size();
+		pp.q = -1;
 
 		for (int i = 0; i <= L + 1; ++i) {
 			if (row[n].e_arr[i]) {
-				primary_list.emplace_back(m, n, row[n].p_arr[i]);
+				primary_list.emplace_back(m, n, row[n].p_arr[i].p, row[n].p_arr[i].q);
 			}
 		}
+	}
+
+	void link_function(SAVE2_type &pp, int m, int n, std::vector<alignment_info> &row, std::vector<SAVE2_type> &secondary_list, int i) {
+		
+		pp.p = row[n].p_arr[0].p;
+		pp.q = secondary_list.size();
+
+		secondary_list.emplace_back(row[n].p_arr[i].p, row[n].p_arr[i].q);
 	}
 
 	int diff(char first, char second) {
@@ -163,7 +190,7 @@ namespace OSALG {
 		int current_index = primary_list.size() - 1;
 		
 		while(current_index > 0) {
-			int next_index = primary_list[current_index].p;
+			int next_index = primary_list[current_index].pointer.p;
 
 			int k, l;
 			if (primary_list[next_index].m - primary_list[next_index].n > primary_list[current_index].m - primary_list[current_index].n) {
@@ -183,7 +210,8 @@ namespace OSALG {
 	int long_gaps_alignment(std::string const &seq1, std::string const &seq2, std::string &cigar, bool extended_cigar) {
 
 		std::vector<SAVE1_type> primary_list;
-		primary_list.emplace_back(0, 0, -1);
+		primary_list.emplace_back(0, 0, -1, -1);
+		std::vector<SAVE2_type> secondary_list;
 
 		short current_row_index = 1;
 		short previous_row_index = 0;
@@ -199,8 +227,11 @@ namespace OSALG {
 				for (int i = 1; i <= L; ++i) {
 					row[current_row_index][n].f_arr[i] = std::min(row[previous_row_index][n].d_arr_val + v[i - 1], row[previous_row_index][n].f_arr[i]) + u[i - 1];
 
-					if (row[previous_row_index][n].d_arr_val + v[i - 1] <= row[previous_row_index][n].f_arr[i]) {
+					if (row[previous_row_index][n].d_arr_val + v[i - 1] < row[previous_row_index][n].f_arr[i]) {
 						row[current_row_index][n].p_arr[i] = row[previous_row_index][n].p_arr[0];
+					}
+					else if (row[previous_row_index][n].d_arr_val + v[i - 1] == row[previous_row_index][n].f_arr[i]) {
+						link_function(row[current_row_index][n].p_arr[i], m - 1, n, row[previous_row_index], secondary_list, i);
 					}
 					else {
 						row[current_row_index][n].p_arr[i] = row[previous_row_index][n].p_arr[i];
@@ -210,9 +241,12 @@ namespace OSALG {
 				//Insertions
 				row[current_row_index][n].f_arr[L + 1] = std::min(row[current_row_index][n - 1].d_arr_val + v[0], row[current_row_index][n - 1].f_arr[L + 1]) + u[0];
 
-				if (row[current_row_index][n - 1].d_arr_val + v[0] <= row[current_row_index][n - 1].f_arr[L + 1]) {
+				if (row[current_row_index][n - 1].d_arr_val + v[0] < row[current_row_index][n - 1].f_arr[L + 1]) {
 					row[current_row_index][n].p_arr[L + 1] = row[current_row_index][n - 1].p_arr[0];
 				} 
+				else if (row[current_row_index][n - 1].d_arr_val + v[0] == row[current_row_index][n - 1].f_arr[L + 1]) {
+					link_function(row[current_row_index][n].p_arr[L + 1], m, n - 1, row[current_row_index], secondary_list, L + 1);
+				}
 				else {
 					row[current_row_index][n].p_arr[L + 1] = row[current_row_index][n - 1].p_arr[L + 1];
 				}
@@ -238,7 +272,7 @@ namespace OSALG {
 			//previous_row = current_row;
 		}
 
-		int p_last;
+		SAVE2_type p_last;
 		adr_function(p_last, seq1.length(), seq2.length(), row[current_row_index], primary_list);
 
 		construct_CIGAR(primary_list, cigar, seq1, seq2, extended_cigar);
